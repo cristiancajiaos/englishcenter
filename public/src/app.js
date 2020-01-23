03b497a2b57ecb7f99914be96933fd2a543507e4
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-01-23 15:32:02 -0300

    Plantilla de AngularJS creada

--- public/src/app.js
+++ public/src/app.js
@@ -1,3 +1,26 @@
-(function(){
-    console.log('Hola, mundo!');
+(function () {
+    var app = angular.module('app', [
+        'ngAnimate',
+        'ngTouch',
+        'ui.bootstrap',
+        'ui.router'
+    ]);
+
+    app.config([
+        '$stateProvider',
+        '$urlRouterProvider',
+        function ($stateProvider, $urlRouterProvider) {
+            var states = [{
+                name: 'main',
+                url: '/main',
+                component: 'mainComponent'
+            }];
+
+            states.forEach(function(state){
+                $stateProvider.state(state);
+            });
+
+            $urlRouterProvider.otherwise('/main');
+        }
+    ]);
 })();
